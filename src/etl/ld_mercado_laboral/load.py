179a8349f8bbe8603ac1f7203a4b12c68fa29ce1
LChaconV <laurachacon.ve@gmail.com>
2026-05-03 12:49:58 -0500

    ajuste del script de carga de mercado_laboral

--- src/etl/ld_mercado_laboral/load.py
+++ src/etl/ld_mercado_laboral/load.py
@@ -8,20 +8,21 @@ TRANSFORM_CONFIG_PATH = PROJECT_ROOT / "config" / "transform" / "mercado_laboral
 create_table_sql = """
 CREATE TABLE IF NOT EXISTS employed_population(
     id_employed_population SERIAL PRIMARY KEY,
-    year INTEGER,
-    id_mun VARCHAR (10),
-    total INTEGER,
-   
-    CONSTRAINT fk_municipio 
-        FOREIGN KEY (id_mun) 
-        REFERENCES dim_divipola(id_mun)
 
-        );
+    year INTEGER,
+    id_mun VARCHAR(10),
+    id_gender VARCHAR(100),
+    total INTEGER,
+
+    CONSTRAINT fk_municipio
+        FOREIGN KEY (id_mun)
+        REFERENCES dim_divipola(id_mun)
+);
 """
 
 create_index_sql = """
 CREATE UNIQUE INDEX IF NOT EXISTS ux_employed_population
-ON employed_population (year, id_mun); 
+ON employed_population (year, id_mun,id_gender); 
 """
 
 def run() -> None:
@@ -35,7 +36,7 @@ def run() -> None:
         create_table_sql=create_table_sql,
         create_index_sql=create_index_sql,
         load_mode="upsert",
-        conflict_columns=["year", "id_mun"],
+        conflict_columns=["year", "id_mun", "id_gender"],
         update_columns=["total"],
         state_field_name="last_incremental_value",
     )
