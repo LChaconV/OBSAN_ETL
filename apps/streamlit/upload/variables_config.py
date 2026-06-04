ce4ce16f842677cb111e56b7792842a3ca435db9
LChaconV <laurachacon.ve@gmail.com>
2026-06-04 13:50:23 -0500

    ajustar campos requeridos en bajo peso al nacer

--- apps/streamlit/upload/variables_config.py
+++ apps/streamlit/upload/variables_config.py
@@ -154,7 +154,7 @@ UPLOAD_VARIABLES: dict = {
             "El archivo debe contener las columnas: **CONSECUTIVE**, **confirmados**, **FEC_NOT**, **COD_PAIS_O**, **COD_DPTO_O**, **COD_MUN_O**"
            
         ),
-        "required_columns": [ "confirmados","COD_PAIS", "COD_DPTO_O", "COD_MUN_O"],
+        "required_columns": [ "confirmados","COD_PAIS_O", "COD_DPTO_O", "COD_MUN_O"],
         "pipeline": "bajo_peso_nacer",
         "format_image": "apps/streamlit/assets/formats/bajo_peso_nacer.png",
         "download_url": "https://portalsivigila.ins.gov.co/Paginas/Buscador.aspx",
