c7a44f8be6df0544660f39d597c96fe7e7ad2bf5
LChaconV <laurachacon.ve@gmail.com>
2026-06-07 17:29:41 -0500

    cambio horario ejecucion ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 const pipelineSchedules = [
-    { name: "api_beneficiarios_iraca", trigger: "cron", hour: 13, minute: 30 }, // Aprox 2 minutos
-    { name: "api_edu_superior", trigger: "cron", hour: 13, minute: 10 }, // Aprox 1 minuto
-    { name: "api_erradicacion_cultivos_coca", trigger: "cron", hour: 13, minute: 15 }, // Aprox 1 minuto
-    { name: "api_indice_riesgo_irca", trigger: "cron", hour: 13, minute: 20 }, // Aprox 1 minuto
-    { name: "api_minerales", trigger: "cron", hour: 13, minute: 25 }, // Aprox 1 minuto
-    { name: "api_produc_gas", trigger: "cron", hour: 13, minute: 30 }, // Aprox 1 minuto
-    { name: "api_produc_petroleo", trigger: "cron", hour: 13, minute: 35 }, // Aprox 1 minuto
-    { name: "api_regalias", trigger: "cron", hour: 13, minute: 40 }, // Aprox 1 minuto
-    { name: "url_terraclimate", trigger: "cron", hour: 14, minute: 0}, // Aprox 1 hora
-    { name: "api_edu_escolar", trigger: "cron", hour: 15, minute: 30 }, // Aprox 2 horas
-    { name: "api_victimas", trigger: "cron", hour: 17, minute: 30 }, // Aprox 3 horas
-    { name: "api_familias_accion", trigger: "cron", hour: 19, minute: 30 }, // Aprox 1 hora
+    { name: "api_beneficiarios_iraca", trigger: "cron", hour: 20, minute: 30 }, // Aprox 2 minutos
+    { name: "api_edu_superior", trigger: "cron", hour: 20, minute: 10 }, // Aprox 1 minuto
+    { name: "api_erradicacion_cultivos_coca", trigger: "cron", hour: 20, minute: 15 }, // Aprox 1 minuto
+    { name: "api_indice_riesgo_irca", trigger: "cron", hour: 20, minute: 20 }, // Aprox 1 minuto
+    { name: "api_minerales", trigger: "cron", hour: 20, minute: 25 }, // Aprox 1 minuto
+    { name: "api_produc_gas", trigger: "cron", hour: 20, minute: 30 }, // Aprox 1 minuto
+    { name: "api_produc_petroleo", trigger: "cron", hour: 20, minute: 35 }, // Aprox 1 minuto
+    { name: "api_regalias", trigger: "cron", hour: 20, minute: 40 }, // Aprox 1 minuto
+    { name: "url_terraclimate", trigger: "cron", hour: 21, minute: 0}, // Aprox 1 hora
+    { name: "api_edu_escolar", trigger: "cron", hour: 22, minute: 30 }, // Aprox 2 horas
+    { name: "api_victimas", trigger: "cron", hour: 1, minute: 30 }, // Aprox 3 horas
+    { name: "api_familias_accion", trigger: "cron", hour: 4, minute: 30 }, // Aprox 1 hora
 ];
 
 module.exports = {
